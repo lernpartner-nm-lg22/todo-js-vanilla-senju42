0881a88924cff5deff4847431ba4050a71d66467
senju42 <142969130+senju42@users.noreply.github.com>
2024-10-21 12:02:35 +0200

    Todo

--- app.js
+++ app.js
@@ -14,13 +14,26 @@ function addGoal() {
   listElement.appendChild(liElement);
 }
 
-buttonElement.addEventListener("click", addGoal);
+// buttonElement.addEventListener("click", addGoal); gleich wie unten
+buttonElement.addEventListener("click", (_) => {
+  const goal = goalElement.value.trim();
+  if (goal === "") {
+    alert("Please enter a goal");
+    return;
+  }
+  const liElement = document.createElement("li");
+  liElement.textContent = goal;
+  listElement.appendChild(liElement);
+});
 
-// Element aus Liste entfernen
+// Element aus Liste erledigen
 function markGoalDone(event) {
   const liElement = event.target;
-  liElement.classList.toggle('done');
+  liElement.classList.toggle("done");
 }
 
-listElement.addEventListener("click", markGoalDone);
-
+// listElement.addEventListener("click", markGoalDone); gleich wie unten
+listElement.addEventListener("click", (event) => {
+  const liElement = event.target;
+  liElement.classList.toggle("done");
+});

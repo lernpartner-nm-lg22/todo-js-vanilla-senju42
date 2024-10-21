0bc45dd6a61b74e67e7dad992886fe799b6803d9
senju42 <142969130+senju42@users.noreply.github.com>
2024-10-21 11:41:05 +0200

    Joki Tutorial

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
-Vue.createApp({
-    data() {
-        return{}
-    },
-    methods: {
+const goalElement = document.getElementById("goal");
+const buttonElement = document.getElementById("submit");
+const listElement = document.getElementById("list");
 
-    }
-}).mount('#app');
+// Element zu Liste Hinzufügen
+function addGoal() {
+  const goal = goalElement.value.trim();
+  if (goal === "") {
+    alert("Please enter a goal");
+    return;
+  }
+  const liElement = document.createElement("li");
+  liElement.textContent = goal;
+  listElement.appendChild(liElement);
+}
+
+buttonElement.addEventListener("click", addGoal);
+
+// Element aus Liste entfernen
+function markGoalDone(event) {
+  const liElement = event.target;
+  liElement.classList.toggle('done');
+}
+
+listElement.addEventListener("click", markGoalDone);
+
